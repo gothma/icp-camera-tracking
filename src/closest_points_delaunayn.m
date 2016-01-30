55a8cef485ff0d2391bc0869d8902cecf2fd16ab
Martin Goth <martin@goth-1.de>
2016-01-31 00:19:01 +0100

    Fix running function for incomplete delaunayn

--- src/closest_points_delaunayn.m
+++ src/closest_points_delaunayn.m
@@ -3,6 +3,11 @@ function [ out ] = closest_points_delaunayn( X, Y )
 %   Detailed explanation goes here
     misc_checkType(X, 'STRUCT(pc)');
     misc_checkType(Y, 'STRUCT(pc)');
-    out = dsearchn(Y.pc, delaunayn(Y.pc), X.pc);
+    
+    try
+        out = dsearchn(Y.pc, delaunayn(Y.pc), X.pc);
+    catch
+        out = dsearchn(Y.pc, X.pc);
+    end
 end
 
--- src/tests/closest_points_delaunayn_test.m
+++ src/tests/closest_points_delaunayn_test.m
@@ -10,11 +10,17 @@ function teardownOnce(testCase)
 teardown()
 end
 
-function test_one_matching(testCase)
-Y = struct();
-Y.pc = [1,2,3];
-X = struct();
+function test_two_matching(testCase)
 X.pc = [1,2,3;4,5,6];
+Y.pc = [7,8,9];
+actual = closest_points_delaunayn(X,Y);
+expected = [1;1];
+verifyEqual(testCase, actual, expected);
+end
+
+function test_one_matching(testCase)
+X.pc = [1,2,3];
+Y.pc = [1,2,3;4,5,6];
 actual = closest_points_delaunayn(X,Y);
 expected = [1];
 verifyEqual(testCase, actual, expected);
