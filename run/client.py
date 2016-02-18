5f981db566bcc45b832e2ea3013ab7c0b665734a
Martin Goth <gothm@cs.uni-freiburg.de>
2016-02-18 18:15:39 +0100

    Distributed workers

---
+++ run/client.py
@@ -0,0 +1,38 @@
+import logging
+import dispy
+import subprocess
+import os
+
+SECRET='f%7!g7OT31Nj6$<faR5"'
+
+def run_matlab(function, args=None):
+    import subprocess
+    if args:
+        arg_str = ', '.join('\'%s\', %s' % (k, repr(v)) for k, v in args.iteritems())
+        call = "%s(%s)" % (function, arg_str)
+    else:
+        call = function
+    print('Calling %s' % call)
+    wrapped_call = 'try; %s; catch; exit; end; exit' % call
+
+
+    total_arglist = ['matlab', '-nodisplay', '-nojvm', '-nosplash', '-r', wrapped_call]
+    subprocess.call(total_arglist)
+
+def main():
+    cluster = dispy.JobCluster(
+        run_matlab,
+        loglevel=logging.DEBUG,
+        node_port=1337,
+        secret=SECRET)
+
+    job = cluster.submit('runtests')
+    res = job()
+    print res
+
+if __name__ == '__main__':
+    main()
+
+
+
+
---
+++ src/icp_worker.m
@@ -0,0 +1,53 @@
+function icp_worker(varargin)
+
+p = inputParser;
+
+p.addOptional('frame', 1);
+
+args = p.parse(varargin{:});
+range = eval(args.frame);
+
+addpath('misc', 'utils');
+
+calibration_matrix = load('../train/camera_calibration_matrix.txt');
+
+camera_images = dir('../train/depth_*.png');
+color_images = dir('../train/color_*.png');
+
+ground_truth = load_ground_truth('../test/ICP_quasi_ground_truth_poses.txt');
+    
+last_pc = [];
+for i=range
+   camera_img_name = camera_images(i).name;
+   if ~isempty(regexpi(camera_img_name, '[0-9]_'))
+      continue 
+   end
+   depth_img = imread(fullfile('../train', camera_img_name));
+   color_img = imread(fullfile('../train', color_images(i).name));
+   
+   camera_rotation = eye(3);
+   camera_transpose = zeros(3, 1);
+   
+   current = loadColorPC(depth_img, color_img, calibration_matrix);
+
+   % do the icp
+   if i > 1
+        [estimated_transformation, errors, rt] = icp_plain(last, current, ...
+        'criterion', @(~,error,steps) steps > 10, 'verbose', false);
+
+        gt_transformation = ominus(ground_truth{i - 1}, ground_truth{i});
+        display(relative_transformation_error(estimated_transformation, gt_transformation));
+
+       % visualize
+       figure,
+           subplot(1,2,1),
+           plot(errors),
+           subplot(1,2,2),
+           current.show(),
+           drawnow
+   end
+    
+   last = current;
+end
+end
+    
