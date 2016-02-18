4b178407dd364611db3b665c310c8460f1d1a7c6
Martin Goth <martin@goth-1.de>
2016-02-18 20:36:29 +0100

    Worker is saving calculated data

--- src/icp_worker.m
+++ src/icp_worker.m
@@ -2,10 +2,10 @@ function icp_worker(varargin)
 
 p = inputParser;
 
-p.addOptional('frame', 1);
+p.addParameter('frame', 1);
 
-args = p.parse(varargin{:});
-range = eval(args.frame);
+p.parse(varargin{:});
+range = eval(p.Results.frame);
 
 addpath('misc', 'utils');
 
@@ -17,6 +17,9 @@ color_images = dir('../train/color_*.png');
 ground_truth = load_ground_truth('../test/ICP_quasi_ground_truth_poses.txt');
     
 last_pc = [];
+last_i = -1;
+results = struct([]);
+
 for i=range
    camera_img_name = camera_images(i).name;
    if ~isempty(regexpi(camera_img_name, '[0-9]_'))
@@ -32,22 +35,28 @@ for i=range
 
    % do the icp
    if i > 1
+        results(i).to_i = i;
+        results(i).from_i = last_i;
+        
         [estimated_transformation, errors, rt] = icp_plain(last, current, ...
         'criterion', @(~,error,steps) steps > 10, 'verbose', false);
 
+        results(i).est_trans = estimated_transformation;
+        results(i).icp_error = errors;
+    
         gt_transformation = ominus(ground_truth{i - 1}, ground_truth{i});
-        display(relative_transformation_error(estimated_transformation, gt_transformation));
-
-       % visualize
-       figure,
-           subplot(1,2,1),
-           plot(errors),
-           subplot(1,2,2),
-           current.show(),
-           drawnow
+        [trans, rot] = relative_transformation_error(estimated_transformation, gt_transformation);
+        results(i).rel_trans_error = trans;
+        results(i).rel_rot_error = rot;
    end
     
    last = current;
+   last_i = i;
 end
+
+result_table = struct2table(results);
+writetable(result_table, 'test.csv');
 end
+
+
     
