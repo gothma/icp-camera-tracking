1cb0407f683ba10bd71145ecdc2342a1498a7458
Martin Goth <martin@goth-1.de>
2016-03-13 22:46:38 +0100

    Reimplement weights

--- src/svd_transformation.m
+++ src/svd_transformation.m
@@ -1,18 +1,20 @@
 function [ transformation, error ] = svd_transformation(X, Y, correspondences, weights, error_func)
 % Returns the transformation from X to Y with minimal error
 
-            mean_X = mean(X.pc);
-            normalized_X = bsxfun(@minus, X.pc, mean_X);
+            normalized_weights = weights ./ sum(weights);
+            mean_X = X.pc' * normalized_weights;
+            normalized_X = X.pc - repmat(mean_X', size(X.pc,1), 1);
+            
 
             P = Y.pc(correspondences, :);
-            mean_P = mean(P);
-            normalized_P = bsxfun(@minus, P, mean_P);
+            mean_P = P' * normalized_weights;
+            normalized_P = (P - repmat(mean_P', size(P,1), 1)) .* repmat(weights, 1, 3);
 
             W = normalized_X' * normalized_P;
             [U, S, V] = svd(W);
             
             R = U * V';
-            t = mean_X - (R * mean_P')';
+            t = mean_X' - (R * mean_P)';
             
             rotated_pc = (R * normalized_P')';
             error = error_func(normalized_X, rotated_pc, S, weights);
